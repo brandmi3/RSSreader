488a93247fb16128b8cc23f1982955c1cfbda315
miros <mejrax@gmail.com>
2018-10-21 14:31:19 +0200

    pridani listeneru na btn a validace

--- src/cz/uhk/fim/rssreader/gui/MainFrame.java
+++ src/cz/uhk/fim/rssreader/gui/MainFrame.java
@@ -5,6 +5,8 @@ import cz.uhk.fim.rssreader.utils.FileUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class MainFrame extends JFrame {
@@ -15,6 +17,7 @@ public class MainFrame extends JFrame {
     private JButton btnLoad;
     private JTextField searchField;
     private JTextArea rssField;
+    private JLabel lblError;
 
     public MainFrame() {
         init();
@@ -33,19 +36,40 @@ public class MainFrame extends JFrame {
         btnLoad = new JButton("Load");
         searchField = new JTextField();
         rssField = new JTextArea();
+        lblError= new JLabel("info: ");
 
         panel.add(btnLoad, BorderLayout.WEST);
         panel.add(searchField, BorderLayout.CENTER);
         panel.add(btnSave, BorderLayout.EAST);
+        panel.add(lblError,BorderLayout.SOUTH);
 
         add(panel, BorderLayout.NORTH);
         add(new JScrollPane(rssField), BorderLayout.CENTER);
 
-        try {
-            rssField.setText(FileUtils.loadStringFromFile("rss_sport.xml"));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        btnLoad.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent m) {
+
+                System.out.println("load");
+                try {
+                    rssField.setText(FileUtils.loadStringFromFile(searchField.getText()));//rss_sport.xml
+                } catch (IOException e) {
+                    searchField.setBackground(Color.pink);
+                    lblError.setText("info: "+e);
+                }
+            }
+        });
+        btnSave.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                System.out.println("save");
+            }
+        });
+
+//todo napsat metodu validateInpu
+//todo pridat JLabel -> Error - red Color
+
 
     }
 }
--- src/cz/uhk/fim/rssreader/utils/FileUtils.java
+++ src/cz/uhk/fim/rssreader/utils/FileUtils.java
@@ -8,11 +8,13 @@ import java.nio.file.Paths;
 public class FileUtils {
 
     public static String loadStringFromFile(String filepath) throws IOException {
-        return  new String (Files.readAllBytes(Paths.get(filepath)));
+        return new String(Files.readAllBytes(Paths.get(filepath)));
     }
 
-    public static void saveStringToFile(String filePath, byte[] data) {
-
+    public static void saveStringToFile(String filePath, byte[] data) throws IOException {
+        Path path = Paths.get(filePath);
+        Files.write(path,data);
+        ;
 
     }
 }
